4ea11a8f8a9c6e7f4998dcdf3f1961b108329aa2
GCODESOfficial <godsgiftetuk2001@gmail.com>
2025-11-14 20:47:13 +0100

    fonts setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-     
+    <div className="font-[Manrope] font-black text-4xl">
+     Next
     </div>
   );
 }
