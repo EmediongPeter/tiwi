95c9b2deca35187598707283329820a1f1496871
GCODESOfficial <godsgiftetuk2001@gmail.com>
2025-11-18 06:55:04 +0100

    herosection fixed

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="relative w-full overflow-hidden bg-[#010501] pt-40 md:pt-68 pb-18 select-none font-normal">
+    <section className="relative z-0 w-full overflow-hidden bg-[#010501] pt-40 md:pt-68 pb-12 select-none font-normal">
       {/* Floating Leaves */}
       <Image
         src="/images/Leave3.svg"
@@ -36,18 +36,16 @@ export default function HeroSection() {
 
       <div className="flex flex-col items-center text-center px-6">
         {/* Pill Tag */}
-        <div className="mb-8  text-[#E7E7E7B2] border border-[#CEFF8B]/20 rounded-full px-6 py-2 text-sm tracking-wide flex items-center gap-2">
+        <div className="mb-8 text-[#E7E7E7B2] border border-[#CEFF8B]/20 rounded-full px-6 py-2 text-sm tracking-wide flex items-center gap-2">
           <img src="/images/sparkle.svg" alt="" className="w-4 h-4" />
           Multi chain. Non custodial. Mobile first. Mainnet ready.
         </div>
 
         {/* Title */}
-
         <div className="relative z-0 text-center font-extrabold leading-tight text-4xl md:text-6xl">
           <h1 className="text-white">
             Powering the{" "}
             <span className="relative inline-block">
-              {/* Highlight rectangle */}
               <span className="absolute inset-0 bg-[#7FB041]/50 h-7 top-1/2 -translate-y-1/2 -z-10"></span>
               Next Evolution
             </span>{" "}
@@ -57,9 +55,9 @@ export default function HeroSection() {
           <h1 className="mt-2">
             <span
               className="
-        bg-gradient-to-r from-[#A8FF61] to-[#4B6E21]
-        bg-clip-text text-transparent
-      "
+                bg-gradient-to-r from-[#A8FF61] to-[#4B6E21]
+                bg-clip-text text-transparent
+              "
             >
               Decentralized Finance
             </span>
@@ -73,18 +71,17 @@ export default function HeroSection() {
         </p>
 
         {/* CTA */}
-
         <button
           className="
-        flex items-center gap-3 text-lg mb-10
-        px-6 py-2
-        rounded-2xl
-        text-white font-medium
-        bg-gradient-to-b from-[#B1F128] via-[#79A726] to-[#466B06]
-        shadow-[0_0_20px_rgba(124,255,88,0.45)]
-        hover:opacity-90
-        transition
-      "
+            flex items-center gap-3 text-lg mb-10
+            px-6 py-2
+            rounded-2xl
+            text-white font-medium
+            bg-gradient-to-b from-[#B1F128] via-[#79A726] to-[#466B06]
+            shadow-[0_0_20px_rgba(124,255,88,0.45)]
+            hover:opacity-90
+            transition
+          "
         >
           Launch App
           <img src="/images/arrowicon.svg" alt="" className="w-6 h-6" />
@@ -131,25 +128,22 @@ export default function HeroSection() {
         </div>
 
         {/* Bottom Ribbon */}
-        <div className="mt-28 relative w-screen flex flex-col justify-center">
-          <div className="w-[140%] rotate-[-7deg] bg-linear-to-r from-[#C5FF85] to-[#7EFF43] py-6 px-4 flex text-black font-semibold text-center">
-            <div>
-              <p className="text-xl">24/7</p>
+        <div className="mt-48 relative w-screen flex flex-col justify-center">
+          <div className="w-[140%] rotate-[4deg] bg-linear-to-r from-[#B1F128]/50 to-[#668B17]/50 h-24 px-4 flex text-black font-semibold text-center"></div>
+
+          <div className="w-[140%] absolute -top-12 rotate-[-4deg] bg-linear-to-r from-[#C5FF85] to-[#7EFF43] py-6 px-[15%] flex gap-52 text-black font-semibold text-center">
+            <div className="text-left">
+              <p className="text-2xl font-bold">24/7</p>
               <p className="text-sm font-normal">Protocol Status</p>
             </div>
-            <div>
-              <p className="text-xl">$10m</p>
+            <div className="text-left">
+              <p className="text-2xl font-bold">$10m</p>
               <p className="text-sm font-normal">Trading Volume</p>
             </div>
-            <div>
-              <p className="text-xl">56+</p>
+            <div className="text-left">
+              <p className="text-2xl font-bold">56+</p>
               <p className="text-sm font-normal">Supported chains</p>
             </div>
-          </div>
-
-
-          <div className="w-[140%] rotate-[10deg] bg-linear-to-r from-[#C5FF85] to-[#7EFF43] h-24 px-4 flex text-black font-semibold text-center">
-            
           </div>
         </div>
       </div>
@@ -166,6 +160,16 @@ export default function HeroSection() {
           onContextMenu={(e) => e.preventDefault()}
         />
       </div>
+
+      {/* Background Image Behind Everything */}
+      <Image
+        src="/images/Herobackground.svg"
+        alt="background"
+        fill
+        className="absolute bottom-0 left-0 object-cover z-[-1]"
+        draggable="false"
+        onContextMenu={(e) => e.preventDefault()}
+      />
     </section>
   );
 }
